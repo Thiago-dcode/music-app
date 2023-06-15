9f6837b7e9d2d8bf19e0f4c0c19a43ab1ac2f4c4
Thiago-dcode <contact.thiago.ferreira@gmail.com>
2023-06-15 12:33:22 +0200

    navBar component

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function NavBar() {
   return (
     <nav className="bg-neutral-900 uppercase font-medium ">
-    <ul className="flex items-center justify-between m-auto text-xl h-16 px-10 text-white max-w-5xl [&>li]:h-full [&>li]:flex [&>li]:items-center">
-      <li >
-        <a className="" href="#">
-          New Keyboard
-        </a>
-      </li>
-      <li>
-        <a href="#">Your keyboards</a>
-      </li>
-    </ul>
-  </nav>
-  )
+      <ul className="flex items-center justify-between m-auto text-xl h-16 px-10 text-white max-w-5xl [&>li]:h-full [&>li]:flex [&>li]:items-center">
+        <li>
+          <a className="" href="#">
+            New Keyboard
+          </a>
+        </li>
+        <li>
+          <a href="#">Your keyboards</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
